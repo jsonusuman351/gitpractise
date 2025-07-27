c7a236929c74aa23b2306dd60ffae5d790e92994
suman jaiswal <jsonusuman351@gmail.com>
2025-07-27 15:01:24 +0530

    updated app.py

--- app.py
+++ app.py
@@ -6,4 +6,9 @@ def subtract(a,b):
     return a - b  
 def multiply(a, b):
     """Returns the product of a and b."""
-    return a * b   
+    return a * b  
+def divide(a, b):
+    """Returns the quotient of a and b."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b     
